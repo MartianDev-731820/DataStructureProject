96a8c5deb6b95fa963d0bae847d7d7adf9aa5594
MartianDev-731820 <khushboo731820@gmail.com>
2024-06-21 15:31:08 -0400

    Bye World by Khushboo

---
+++ src/main/java/com/java/web/Khushboo/AndYou.java
@@ -0,0 +1,8 @@
+package com.java.web.Khushboo;
+
+public class AndYou {
+	public static void main(String[] args) {
+		System.out.println("And You by Khushboo");
+	}
+
+}
---
+++ src/main/java/com/java/web/Khushboo/HowAreyou.java
@@ -0,0 +1,8 @@
+package com.java.web.Khushboo;
+
+public class HowAreyou {
+	public static void main(String[] args) {
+		System.out.println("How Are you by Khushboo");
+	}
+
+}
